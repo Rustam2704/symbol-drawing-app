7c2de8d5790568fe5dd58c0863ceff34a5546112
Rustam Alieskerov <rustam@example.com>
2026-07-14 02:10:24 +0300

    Remove archived project debris

--- archive/vendor-source/perfect-freehand/commitlint.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Customize rules as needed
-    'body-max-line-length': [1, 'always', 100], // Warn instead of error
-    'footer-max-line-length': [1, 'always', 100],
-  },
-}
